9c0ed1c4e07e355484e773f29e0979425989d658
Stanley Filipe Neiva Andrade e Silva <stanley92filipe@gmail.com>
2023-01-02 18:24:32 -0300

    feat: global styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import ReactDOM from 'react-dom/client'
 import { Routes } from './routes'
+import { GlobalStyle } from './styles/globalStyle'
+
+function App (): JSX.Element {
+  return (
+    <Fragment>
+      <Routes />
+      <GlobalStyle />
+    </Fragment>
+  )
+}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Routes />
+    <App />
   </React.StrictMode>
 )
---
+++ frontend/src/styles/globalStyle.ts
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :root {
+    --background-color: #ffffff;
+    --primary-color: #1BA2A1;
+    --text-color: #474747;
+  }
+
+  body {
+    min-height: 100vh;
+    min-width: 100vw;
+
+    background-color: var(--background-color);
+  }
+`
