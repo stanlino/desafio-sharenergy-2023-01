45cbe54d6563772aaebf6bab8c0374ae9b711a05
Stanley Filipe Neiva Andrade e Silva <stanley92filipe@gmail.com>
2023-01-02 19:32:19 -0300

    feat: login screen

--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -1,5 +1,50 @@
+import { FormEvent, useRef } from 'react'
+import { BiUserCircle, BiLockAlt } from 'react-icons/bi'
+import { Button, Container, FormHeader } from './styles'
+
 export function LoginScreen (): JSX.Element {
+  const usernameInputRef = useRef<HTMLInputElement>(null)
+  const passwordInputRef = useRef<HTMLInputElement>(null)
+
+  const handleSubmit = (event: FormEvent): void => {
+    event.preventDefault()
+
+    if ((usernameInputRef.current == null) || (passwordInputRef.current == null)) return
+
+    const { value: username } = usernameInputRef.current
+    const { value: password } = passwordInputRef.current
+
+    console.log(username, password)
+  }
+
   return (
-    <h1>LoginScreen</h1>
+    <Container>
+      <main>
+        <form onSubmit={handleSubmit}>
+          <FormHeader>
+            <h2>Desafio</h2>
+            <img src="/shareenergylogo.png" alt="Share Energy Logo" />
+          </FormHeader>
+
+          <label className='input-text-label' htmlFor="username-input">
+            <BiUserCircle />
+            <input required ref={usernameInputRef} placeholder='Usuário' type="text" id="username-input" />
+          </label>
+          <label className='input-text-label' htmlFor="password-input">
+            <BiLockAlt />
+            <input required ref={passwordInputRef} placeholder='Senha' type="password" id="password-input" />
+          </label>
+          <label className='input-checkbox-label' htmlFor="remember-me-checkbox">
+            <input type="checkbox" id="remember-me-checkbox" />
+            <span>Lembre-se de mim</span>
+          </label>
+
+          <Button>
+            Acessar
+          </Button>
+
+        </form>
+      </main>
+    </Container>
   )
 }
---
+++ frontend/src/pages/login/styles.ts
@@ -0,0 +1,124 @@
+import { lighten } from 'polished'
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  flex: 1;
+
+  user-select: none;
+
+  main {
+    display: flex;
+    flex: 1;
+
+    max-width: 1200px;
+    margin: 0 auto;
+
+    justify-content: center;
+    align-items: center;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+
+    border: 1px solid #ececec;
+
+    padding: 8px 16px;
+
+    width: min(400px, 100%);
+    border-radius: 8px;
+
+    @media (max-width: 400px) {
+      border: 0;
+    }
+
+    .input-text-label {
+      background-color: #D9D9D9;
+      margin: 4px 0;
+
+      display: flex;
+      align-items: center;
+
+      width: 100%;
+      gap: 0.5rem;
+
+      height: 40px;
+      border-radius: 8px;
+      padding: 0 8px;
+
+      svg {
+        font-size: 25px;
+        color: #797979;
+      }
+
+      input {
+        border: 0;
+        display: flex;
+        flex: 1;
+
+        outline: 0;
+        background-color: transparent;
+
+        font-size: 14px;
+        line-height: 14px;
+
+        ::placeholder {
+          color: #797979;
+        }
+      }
+    }
+
+    .input-checkbox-label {
+      display: flex;
+      flex-direction: row;
+
+      cursor: pointer;
+
+      margin: 8px 0;
+
+      span {
+        margin-left: 8px;
+      }
+    }
+  }
+`
+
+export const FormHeader = styled.header`
+  
+  display: flex;
+  flex-direction: column;
+
+  align-items: center;
+
+  margin: 2rem 0;
+
+  h2 {
+    color: var(--text-color)
+  }
+  
+  img {
+    width: 200px;
+  }
+`
+
+export const Button = styled.button.attrs({
+  type: 'submit'
+})`
+  background-color: var(--primary-color);
+  color: white;
+  height: 40px;
+  outline: 0;
+  border: 0;
+  border-radius: 8px;
+
+  font-size: 18px;
+
+  cursor: pointer;
+
+  transition: all .4s;
+
+  &:hover {
+    background-color: ${lighten(0.1, '#1BA2A1')};
+  }
+`
