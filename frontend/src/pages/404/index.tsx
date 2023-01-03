fd8f903b0ff71ee76faae9967737beeac9dfeec1
Stanley Filipe Neiva Andrade e Silva <stanley92filipe@gmail.com>
2023-01-03 14:28:59 -0300

    feat: 404 page

---
+++ frontend/src/pages/404/index.tsx
@@ -0,0 +1,10 @@
+import { Container } from './styles'
+
+export function NotFoundPage (): JSX.Element {
+  return (
+    <Container>
+      <h1>Error 404</h1>
+      <span>Page not found</span>
+    </Container>
+  )
+}
---
+++ frontend/src/pages/404/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const Container = styled.main`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+
+  justify-content: center;
+  align-items: center;
+
+  h1 {
+    color: var(--text-color);
+    font-size: 3rem;
+  }
+`
