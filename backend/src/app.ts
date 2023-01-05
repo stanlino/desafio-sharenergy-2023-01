328395acfbc2976a1d37f00903e692e165611670
Stanley Filipe Neiva Andrade e Silva <stanley92filipe@gmail.com>
2023-01-05 10:03:45 -0300

    backend: hello world

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,1 +1,13 @@
-console.log('jonas');
+import express from 'express';
+
+const app = express();
+
+const PORT = process.env.PORT || 3333;
+
+app.get('/', (req, res) => {
+  return res.send('hello world');
+});
+
+app.listen(PORT, () => {
+  console.log('🔥 Servidor rodando na porta 3333');
+});
