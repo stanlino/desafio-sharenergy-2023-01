9cc93e9c3107388b013341b8be893909eb7e3064
Stanley Filipe Neiva Andrade e Silva <stanley92filipe@gmail.com>
2023-01-06 17:40:01 -0300

    backend: cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import "reflect-metadata";
 import "express-async-errors"
 import 'dotenv/config'
 
+import cors from 'cors'
 import express from 'express';
 
 import './shared/container'
@@ -10,6 +11,7 @@ import { errorsMonitor } from './errors';
 
 const app = express();
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 app.use(errorsMonitor)
